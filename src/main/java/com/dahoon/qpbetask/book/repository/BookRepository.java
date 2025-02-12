18c79a5bb4e3c7ffa1b21dd4323adba7d8587cf2
dahoon kang <dahoon7151@gmail.com>
2025-02-12 23:48:58 +0900

    도서 CRUD 및 필터링 구현, 테스트 완료

--- src/main/java/com/dahoon/qpbetask/book/repository/BookRepository.java
+++ src/main/java/com/dahoon/qpbetask/book/repository/BookRepository.java
@@ -4,10 +4,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findAll(Pageable pageable);
 
     Optional<Book> findById(Long id);
+
+    List<Book> findByTitleContaining(String title);
+
+    List<Book> findByAuthorContaining(String author);
 }
