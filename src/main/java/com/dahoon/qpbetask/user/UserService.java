176a5c5679b25aa88bf12e85e84a665fa01480c4
dahoon kang <dahoon7151@gmail.com>
2025-02-14 22:04:09 +0900

    User, Loan 관련 API 캐싱 적용

--- src/main/java/com/dahoon/qpbetask/user/UserService.java
+++ src/main/java/com/dahoon/qpbetask/user/UserService.java
@@ -1,14 +1,19 @@
 package com.dahoon.qpbetask.user;
 
+import com.dahoon.qpbetask.common.cache.CacheInvalidationPublisher;
 import com.dahoon.qpbetask.user.component.JwtTokenProvider;
 import com.dahoon.qpbetask.user.dto.JwtTokenDto;
 import com.dahoon.qpbetask.user.dto.UserDto;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.util.List;
 
@@ -20,8 +25,10 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
+    private final CacheInvalidationPublisher cacheInvalidationPublisher;
 
     @Transactional
+    @CacheEvict(value = "users", allEntries = true)
     public UserDto join(UserDto userDto) {
         if (userRepository.existsByUsername(userDto.getUsername())) {
             throw new IllegalArgumentException("이미 등록된 사용자 이름입니다.");
@@ -32,8 +39,17 @@ public class UserService {
                 .password(passwordEncoder.encode(userDto.getPassword()))
                 .build();
         log.info("User 객체 생성");
+        UserDto savedUserDto = UserDto.toDto(userRepository.save(user));
 
-        return UserDto.toDto(userRepository.save(user));
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+            @Override
+            public void afterCommit() {
+                log.info("캐시 afterCommit");
+                cacheInvalidationPublisher.publishInvalidationMessage("users");
+            }
+        });
+
+        return savedUserDto;
     }
 
     @Transactional
@@ -52,6 +68,7 @@ public class UserService {
     }
 
     @Transactional
+    @CacheEvict(value = "refreshTokens", key = "#jwtTokenDto.refreshToken")
     public JwtTokenDto refreshToken(JwtTokenDto jwtTokenDto) {
         if (!jwtTokenProvider.validateToken(jwtTokenDto.getRefreshToken())) {
             throw new IllegalArgumentException("유효하지 않은 RefreshToken입니다.");
@@ -69,6 +86,7 @@ public class UserService {
     }
 
     @Transactional
+    @CacheEvict(value = "refreshTokens", key = "#username")
     public void logout(String username) {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 사용자입니다."));
@@ -77,7 +95,7 @@ public class UserService {
         userRepository.save(user);
     }
 
-    @Transactional
+    @Cacheable(value = "users")
     public List<UserDto> showUserList() {
         List<User> userList = userRepository.findAll();
         return userList.stream()
@@ -85,7 +103,7 @@ public class UserService {
                 .toList();
     }
 
-    @Transactional
+    @Cacheable(value = "user", key = "#id")
     public UserDto showUser(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("해당 ID의 사용자가 없습니다."));
